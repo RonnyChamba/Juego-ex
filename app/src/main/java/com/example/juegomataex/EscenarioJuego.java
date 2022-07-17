3af60d62616bcc57c5a1d183528d69969a07f7b6
Ronny <ronnychamba96@gmail.com>
2022-07-17 18:50:35 -0500

    perfil jugador

---
+++ app/src/main/java/com/example/juegomataex/EscenarioJuego.java
@@ -0,0 +1,14 @@
+package com.example.juegomataex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EscenarioJuego extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escenario_juego);
+    }
+}
--- app/src/main/java/com/example/juegomataex/Menu.java
+++ app/src/main/java/com/example/juegomataex/Menu.java
@@ -1,20 +1,43 @@
 package com.example.juegomataex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class Menu extends AppCompatActivity {
 
     Button btnCerrarSesion;
     FirebaseAuth auth;
     FirebaseUser user;
+
+    Button btnJugar;
+    Button btnPuntuacion;
+    Button btnAcercaDe;
+
+    TextView txtTituloMenu;
+    TextView txtUidJugadorMenu;
+    TextView txtZombieMenu;
+    TextView txtSubTituloMenu;
+    TextView txtCorreoJugadorMenu;
+    TextView txtNombreJugadorMenu;
+
+    FirebaseDatabase database;
+    DatabaseReference jugadores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +46,55 @@ public class Menu extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
+        txtTituloMenu = findViewById(R.id.txtTituloMenu);
+        txtUidJugadorMenu = findViewById(R.id.txtUidMenu);
+        txtSubTituloMenu = findViewById(R.id.txtSubtituloBotones);
+        txtCorreoJugadorMenu = findViewById(R.id.txtCorreoMenu);
+        txtNombreJugadorMenu = findViewById(R.id.txtNombreMenu);
+        txtZombieMenu = findViewById(R.id.txtZombies);
 
+        btnJugar = findViewById(R.id.btnJugar);
+        btnPuntuacion = findViewById(R.id.btnPuntaciones);
+        btnAcercaDe = findViewById(R.id.btnHacerca);
         btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
 
+        database = FirebaseDatabase.getInstance();
+        jugadores = database.getReference("MI DATA BASE JUGADORES");
+
         btnCerrarSesion.setOnClickListener((event) -> cerrarSesion());
+
+        btnJugar.setOnClickListener((event) -> {
+            Intent intent = new Intent(Menu.this, EscenarioJuego.class);
+            String nombre = txtNombreJugadorMenu.getText() +"";
+            String uId = txtUidJugadorMenu.getText() +"";
+            String email = txtCorreoJugadorMenu.getText() +"";
+            String zombies = txtZombieMenu.getText() +"";
+
+            intent.putExtra("uId", uId);
+            intent.putExtra("nombres", nombre);
+            intent.putExtra("email", email);
+            intent.putExtra("zombie", zombies);
+            startActivity(intent);
+
+
+        });
+
+
+        btnCerrarSesion.setOnClickListener((event) -> cerrarSesion());
+
+        Typeface typeface = Typeface.createFromAsset(Menu.this.getAssets(), "fuentes/zombie.TTF");
+
+        btnJugar.setTypeface(typeface);
+        btnPuntuacion.setTypeface(typeface);
+        btnAcercaDe.setTypeface(typeface);
+        btnCerrarSesion.setTypeface(typeface);
+
+        txtTituloMenu.setTypeface(typeface);
+        txtZombieMenu.setTypeface(typeface);
+        txtSubTituloMenu.setTypeface(typeface);
+        txtCorreoJugadorMenu.setTypeface(typeface);
+        txtNombreJugadorMenu.setTypeface(typeface);
+
     }
     private void cerrarSesion() {
         auth.signOut();
@@ -44,10 +112,39 @@ public class Menu extends AppCompatActivity {
     private void usuarioLogeado() {
 
         if (user != null) {
+            consulta();
             Toast.makeText(this, "Jugador en linea", Toast.LENGTH_LONG).show();
         } else {
             startActivity(new Intent(this, MainActivity.class));
             finish();
         }
     }
+
+    private void consulta() {
+        Query query = jugadores.orderByChild("Email").equalTo(user.getEmail());
+
+        query.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+
+                    String email = ds.child("Email").getValue() + "";
+                    String uId = ds.child("Uid").getValue() + "";
+                    String nombres = ds.child("Nombres").getValue() + "";
+                    String zombies = ds.child("Zombies").getValue() + "";
+                    txtCorreoJugadorMenu.setText(email);
+                    txtNombreJugadorMenu.setText(nombres);
+                    txtZombieMenu.setText(zombies);
+                    txtUidJugadorMenu.setText(uId);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
