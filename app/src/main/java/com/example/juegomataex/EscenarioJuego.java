a11360124347811cab01adbf67c1fd2ea6dca6d1
Ronny <ronnychamba96@gmail.com>
2022-07-18 12:08:52 -0500

    evento killer

--- app/src/main/java/com/example/juegomataex/EscenarioJuego.java
+++ app/src/main/java/com/example/juegomataex/EscenarioJuego.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class EscenarioJuego extends AppCompatActivity {
 
     String nombre,email, uId, zombie;
     TextView txtContador, txtNombre, txtTiempo;
+    ImageView imgZombie;
+    int contador = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +24,7 @@ public class EscenarioJuego extends AppCompatActivity {
         txtNombre = findViewById(R.id.txtNombreEs);
         txtContador = findViewById(R.id.txtContadorEsc);
         txtTiempo = findViewById(R.id.txtTiempoEsc);
+        imgZombie = findViewById(R.id.imgJuego);
 
         Bundle intent = getIntent().getExtras();
 
@@ -27,6 +34,17 @@ public class EscenarioJuego extends AppCompatActivity {
         zombie= intent.getString("zombie");
         txtNombre.setText(nombre);
         txtContador.setText(zombie);
+
+        imgZombie.setOnClickListener( (click) ->{
+            contador++;
+            txtContador.setText(String.valueOf(contador));
+            imgZombie.setImageResource(R.drawable.tumba);
+
+            new Handler().postDelayed((() -> {
+                imgZombie.setImageResource(R.drawable.icono_ex);
+            }), 500);
+        });
+
 
         Typeface fuente = Typeface.createFromAsset(EscenarioJuego.this.getAssets(), "fuentes/zombie.TTF");
         txtNombre.setTypeface(fuente);
